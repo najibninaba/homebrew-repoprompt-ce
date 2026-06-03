4df932361ceac0d5f0ac9d943254ade20c0c161b
Najib Ninaba <najibninaba@gmail.com>
2026-06-03 11:40:40 +0800

    Add RepoPrompt CE cask

---
+++ Casks/repoprompt-ce.rb
@@ -0,0 +1,29 @@
+cask "repoprompt-ce" do
+  version "1.0.3,4"
+  sha256 "61612298ccb4ecb03a724c0dfeaec1b27eadb6635879bcd59152855f3cb2a9ff"
+
+  url "https://github.com/repoprompt/repoprompt-ce-updates/releases/download/v#{version.csv.first}/RepoPrompt-#{version.csv.first}-#{version.csv.second}.zip"
+  name "RepoPrompt CE"
+  desc "Open-source community edition of RepoPrompt"
+  homepage "https://github.com/repoprompt/repoprompt-ce"
+
+  livecheck do
+    url "https://github.com/repoprompt/repoprompt-ce-updates/releases/latest/download/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: :sonoma
+
+  app "RepoPrompt.app", target: "RepoPrompt CE.app"
+
+  uninstall quit: "com.pvncher.repoprompt.ce"
+
+  zap trash: [
+    "~/Library/Application Support/RepoPrompt CE",
+    "~/Library/Caches/com.pvncher.repoprompt.ce",
+    "~/Library/HTTPStorages/com.pvncher.repoprompt.ce",
+    "~/Library/Preferences/com.pvncher.repoprompt.ce.plist",
+    "~/Library/Saved Application State/com.pvncher.repoprompt.ce.savedState",
+  ]
+end
